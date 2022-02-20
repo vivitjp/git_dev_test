3dfd45be34838114b791f8bdbc6de121f0846c93
vivit <kenji@vivitjp.com>
2022-02-21 06:51:45 +0900

    dev 2 commit

---
+++ src/components/List3.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+export const List1 = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>LIST3</p>
+      </header>
+    </div>
+  );
+};
